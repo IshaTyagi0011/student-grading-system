9cc2e76a3f1efd10e767de3ef351ecd5738781a6
Tyagiisha <147482741+Tyagiisha@users.noreply.github.com>
2024-06-24 18:21:12 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,23 @@
+ class StudentGradingSystem {
+    public static void main(String[] args) {
+        int[] studentMarks = {85, 92, 58, 77, 69};
+        for (int marks : studentMarks) {
+            char grade = calculateGrade(marks);
+            System.out.println("Student with marks " + marks + " gets grade: " + grade);
+        }
+    }
+
+    public static char calculateGrade(int marks) {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 80) {
+            return 'B';
+        } else if (marks >= 70) {
+            return 'C';
+        } else if (marks >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
